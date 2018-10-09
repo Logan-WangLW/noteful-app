a2c38ffacd2f6c1ac00182ea5d00b3ea3e2cf1ee
Logan Wang <loganxwang@hotmail.com>
2018-10-09 11:34:12 -0700

    error handling middleware

--- server.js
+++ server.js
@@ -5,8 +5,12 @@ const express = require('express');
 const data = require('./db/notes');
 // Different exported modules
 const {PORT} = require('./config');
+const logger = require('./middleware/logger');
 // INSERT EXPRESS APP CODE HERE...
 const app = express();
+// log requests
+app.use(logger);
+// create static web server
 app.use(express.static('public'));
 
 app.get('/api/notes', (req, res) => {
@@ -32,6 +36,24 @@ app.get('/api/notes', (req, res) => {
 app.get('/api/notes/:id', (req, res) => {
   const id = req.params.id;
   res.json(data.find(item => item.id === Number(id)));
+});
+// error example
+app.get('/boom', (req, res, next) => {
+  throw new Error('Boom!!');
+});
+
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  res.status(404).json({ message: 'Not Found' });
+});
+
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err
+  });
 });
 
 // ADD STATIC SERVER HERE
