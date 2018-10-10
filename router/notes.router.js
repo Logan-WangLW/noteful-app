1a084a493a4f5b0836c286e86ef15e4ffa8f16d1
Logan Wang <loganxwang@hotmail.com>
2018-10-10 13:34:51 -0700

    completed v3

--- router/notes.router.js
+++ router/notes.router.js
@@ -110,4 +110,16 @@ router.post('/notes', (req, res, next) => {
   });
 });
 
+// Delete an item
+router.delete('/notes/:id', (req, res, next) => {
+  const reqId = req.params.id; 
+
+  notes.delete(reqId, (err) =>{
+    if (err){
+      return next(err);
+    }
+    res.sendStatus(204);
+  });
+});
+
 module.exports = router;
